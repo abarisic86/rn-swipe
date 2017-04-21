65ed6511c865a1e052b6d454ede7dc1eefdab8c4
abarisic86 <ante.barisic@gmail.com>
2017-04-22 01:02:07 +0200

    data

---
+++ src/Deck.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Animated } from 'react-native'
+
+class Deck extends Component {
+  render() {
+    return (
+      <Animated.View>
+
+      </Animated.View>
+    )
+  }
+}
+
+export default Deck
