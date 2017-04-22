39ae623bd793d3ce4fa3248a75a593e317fc2c62
abarisic86 <ante.barisic@gmail.com>
2017-04-22 02:43:41 +0200

    reorganize

--- src/Deck.js
+++ src/Deck.js
@@ -11,55 +11,6 @@ const SWIPE_THRESHOLD = 0.35 * SCREEN_WIDTH
 const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
-  renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
-        return (
-          <Animated.View
-            key={item.id}
-            style={this.getCardStyle()}
-            {...this.state.panResponder.panHandlers}>
-            {this.props.renderCard(item)}
-          </Animated.View>
-        )
-      }
-      return this.props.renderCard(item)
-    })
-  }
-
-  getCardStyle() {
-    const { position } = this.state
-
-    const rotate = position.x.interpolate({
-      inputRange: [SCREEN_WIDTH, 0, SCREEN_WIDTH],
-      outputRange: ['-30deg', '0deg', '30deg']
-    })
-
-    return {
-      ... this.state.position.getLayout(),
-      transform: [{ rotate: rotate }]
-    }
-
-  }
-
-  forceSwipe(direction) {
-    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
-
-    Animated.timing(this.state.position, {
-      toValue: { x, y: 0 },
-      duration: SWIPE_OUT_DURATION
-    }).start(() => this.onSwipeComplete())
-  }
-
-  onSwipeComplete() {
-
-  }
-
-  resetPosition() {
-    Animated.spring(this.state.position, {
-      toValue: { x: 0, y: 0 }
-    }).start()
-  }
 
   constructor(props) {
     super(props)
@@ -82,8 +33,63 @@ class Deck extends Component {
       }
     })
 
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
   }
+
+
+  renderCards() {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}>
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
+      return this.props.renderCard(item)
+    })
+  }
+
+  getCardStyle() {
+    const { position } = this.state
+
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-30deg', '0deg', '30deg']
+    })
+
+    return {
+      ... this.state.position.getLayout(),
+      transform: [{ rotate: rotate }]
+    }
+
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction))
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props
+    const item = data[this.state.index]
+
+    direction === 'right' ? onSwipeRight(item): onSwipeLeft(item)
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start()
+  }
+
 
   render() {
     return (
