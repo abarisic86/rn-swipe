ecdb4560d87b8e03e79ac4c15ef0b46ffaa9ba81
abarisic86 <ante.barisic@gmail.com>
2017-04-22 02:34:47 +0200

    swipe

--- src/Deck.js
+++ src/Deck.js
@@ -7,6 +7,8 @@ import {
 } from 'react-native'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_THRESHOLD = 0.35 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
   renderCards() {
@@ -15,9 +17,9 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={ this.getCardStyle()}   
+            style={this.getCardStyle()}
             {...this.state.panResponder.panHandlers}>
-              {this.props.renderCard(item)}
+            {this.props.renderCard(item)}
           </Animated.View>
         )
       }
@@ -26,39 +28,67 @@ class Deck extends Component {
   }
 
   getCardStyle() {
-    const {position} = this.state
+    const { position } = this.state
 
     const rotate = position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
-      outputRange: ['-30deg','0deg','30deg']
+      inputRange: [SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-30deg', '0deg', '30deg']
     })
 
     return {
       ... this.state.position.getLayout(),
-      transform: [{rotate: rotate}]
+      transform: [{ rotate: rotate }]
     }
 
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete())
+  }
+
+  onSwipeComplete() {
+
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start()
   }
 
   constructor(props) {
     super(props)
 
-    const position  = new Animated.ValueXY()
+    const position = new Animated.ValueXY()
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        position.setValue({x: gesture.dx})
+        position.setValue({ x: gesture.dx })
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right')
+          console.log('YES')
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left')
+        } else {
+          this.resetPosition(event, gesture);
+        }
+      }
     })
- 
+
     this.state = { panResponder, position };
   }
 
   render() {
     return (
       <View>
-          {this.renderCards()}
+        {this.renderCards()}
       </View>
     )
   }
